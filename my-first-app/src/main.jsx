c27f7d6f6e746b381b25dc88958cc42384538e4f
Emma McEvoy <62438729+EmmaM17@users.noreply.github.com>
2025-10-23 12:39:04 +0100

    Changes in main and index

--- my-first-app/src/main.jsx
+++ my-first-app/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/* import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './scripts/HelloWorld.jsx'
@@ -7,4 +7,31 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+) */
+
+import { createRoot } from 'react-dom/client'
+/* createRoot(document.getElementById('root')).render(
+  <h1>Hello React!</h1>
+)   */
+
+/* createRoot(document.getElementById('root')).render(
+  <p>Welcome!</p>
+)  */
+
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
+createRoot(document.getElementById('root')).render(
+  myelement
 )
