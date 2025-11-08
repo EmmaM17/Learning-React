8303515232e2b10d6a9a8041b622a1920abe853e
Emma McEvoy <62438729+EmmaM17@users.noreply.github.com>
2025-11-08 12:04:17 +0000

    Click updates question
    Clicking the button now shows the next questions and answers

--- movie-suggestion/src/Home.jsx
+++ movie-suggestion/src/Home.jsx
@@ -4,7 +4,34 @@ import viteLogo from '/vite.svg'
 import './Home.css'
 
 function Home() {
-  let moods = ['Scary', 'Happy', 'Romantic', 'Thrilling', 'Sad'];
+  
+  // Logic
+ const [questionNum, setQuestionNum] = useState(1);
+
+ function getQuestion() {
+    if (questionNum === 1) {
+      return "Choose the mood";
+    }
+    else if (questionNum === 2){
+      return "Choose an Actor";
+    }
+    return "";
+  }
+
+  function getAnswer(){
+    if (questionNum === 1){
+      let answers = ['Scary', 'Happy', 'Romantic', 'Thrilling', 'Sad'];
+      return answers;
+    }
+    else if(questionNum === 2){
+      let answers = ['Brad Pitt', 'Timothee Chalamet', 'Bruce Willis', 'Angelina Jolie', 'Jennifer Lawrence'];
+      return answers;
+    }
+    return[]
+  }
+  
+
+  // Display
   return (
     <div className="parent">
       <title>Movie Suggestions</title>
@@ -15,8 +42,12 @@ function Home() {
 
       <div className= "quiz">
         <div className='form'>
-          <h2>Choose the mood</h2>
-          <ul>{moods.map((mood) => <li>{ mood }</li>)}</ul>
+          <h2>{getQuestion()}</h2>
+          <button className="mood" onClick={() => setQuestionNum(questionNum + 1)}>{getAnswer()[0]}</button>
+          <button className="mood" onClick={() => setQuestionNum(questionNum + 1)}>{getAnswer()[1]}</button>
+          <button className="mood" onClick={() => setQuestionNum(questionNum + 1)}>{getAnswer()[2]}</button>
+          <button className="mood" onClick={() => setQuestionNum(questionNum + 1)}>{getAnswer()[3]}</button>
+          <button className="mood" onClick={() => setQuestionNum(questionNum + 1)}>{getAnswer()[4]}</button>
         </div>
       </div> {/* End of quiz class*/}
 
